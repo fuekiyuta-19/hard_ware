78a8ea64b8cddc1dce696d0c1ccc78dfe567f0d4
fuekiyuta-19 <64128823+fuekiyuta-19@users.noreply.github.com>
2021-12-06 18:03:09 +0900

    sumple for struct

--- for_study_cpp/structure.cpp
+++ for_study_cpp/structure.cpp
@@ -7,25 +7,11 @@ struct product
     int stock;
 };
 
-void set_param(product name, int i, int j, int k)
-{
-    name.id    = i;
-    name.price = j;
-    name.stock = k;
-}    
-
 int main()
 {
-    // product pen;
-    // pen.id    = 0;
-    // pen.price = 100;
-    // pen.stock = 200;
-    product pen = set_param(pen, 0, 100, 200);
+    product pen = {0, 100, 200};
 
-    product car;
-    car.id    = 1;
-    car.price = 10000;
-    car.stock = 2;
+    product car = {1, 10000, 2};
 
     product * ptr  = &pen;
     product * cptr = &car;
