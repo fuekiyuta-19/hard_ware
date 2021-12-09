b38c28d472201ded0828095e8eefd9489ce1e5d6
fuekiyuta-19 <64128823+fuekiyuta-19@users.noreply.github.com>
2021-12-09 17:13:19 +0900

    sumple for using

---
+++ for_study_cpp/define.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using integer = int;
+
+integer main()
+{
+    int value = 42;
+
+    using number = integer;
+
+    number num = 0;
+
+    value = num;
+
+    std::cout << value << std::endl;
+}
