87ddddf9b2557a1dc2066a38423bc9d85825616b
fuekiyuta-19 <64128823+fuekiyuta-19@users.noreply.github.com>
2021-12-31 22:04:41 +0900

    sumple for constructer

---
+++ for_study_cpp/constructer.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+
+class person
+{
+    std::string m_name;
+    int m_age;
+
+public:
+    person();
+
+    void set_name(std::string name);
+    void set_age(int age);
+
+    std::string name() const;
+    int age() const;
+};
+
+//person class内のageを-1と初期化
+person::person() : m_age(-1)
+{
+    std::cout << "Called from constructer" << std::endl;
+}
+
+void person::set_name(std::string name)
+{
+    m_name = name;
+}
+
+void person::set_age(int age)
+{
+    m_age = age;
+}
+
+std::string person::name() const
+{
+    return m_name;
+}
+
+int person::age() const
+{
+    return m_age;
+}
+
+int main()
+{
+    person bob;
+
+    std::cout << "Age after initialize" << bob.age() << std::endl;
+    bob.set_name("BOB");
+    bob.set_age(20);
+    std::cout << "Name is " << bob.name() << std::endl;
+    std::cout << "Age is " << bob.age() << std::endl;
+
+}
