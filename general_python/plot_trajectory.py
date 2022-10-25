59e02b1d9bce155cf4698bece0d005f6f7eabb05
fuekiyuta-19 <64128823+fuekiyuta-19@users.noreply.github.com>
2022-10-25 15:31:29 +0900

    [add]function to plot the ship shape

---
+++ general_python/plot_trajectory.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+
+from subroutine import font_set, surrounding
+
+filepath = "general_python/datalist/"
+dataname = ["TM_TAKAOKI_NNberthing_time_25-Oct-2022_13_38_49",
+            "TM_TAKAOKI_NNberthing_time_25-Oct-2022_13_42_05",
+            "TM_TAKAOKI_NNberthing_time_25-Oct-2022_13_45_18",
+            "TM_TAKAOKI_NNberthing_time_25-Oct-2022_13_47_37"]
+
--- general_python/subroutine/surrounding.py
+++ general_python/subroutine/surrounding.py
@@ -11,3 +11,31 @@ def env():
 
     return berth_x, berth_y, surroundings_x, surroundings_y
 
+def ship_shape_plot(x, y, psi, ship_type) : # 0:esso, 1:takaoki
+    pp_takaoki_CSV  = pd.read_csv('general_python/datalist/input_CSV_VR/principal_particulars_cement.csv', index_col = 0)
+    esso_lpp        = 3.0
+    esso_breadth    = 0.48925
+    takaoki_lpp     = pp_takaoki_CSV.at['lpp', 'value']
+    takaoki_breadth = pp_takaoki_CSV.at['breadth', 'value']
+#     ref
+    if ship_type == 0 :
+        lpp     = esso_lpp
+        breadth = esso_breadth
+        
+    elif ship_type == 1 :
+        lpp       = takaoki_lpp
+        breadth   = takaoki_breadth
+        
+#     shipshape_Y   = np.array([-lpp / 2, lpp / 4, lpp / 2, lpp / 4, -lpp / 2, -lpp / 2])
+#     shipshape_X   = np.array([-breadth / 2, -breadth / 2, 0, breadth / 2, breadth / 2, -breadth / 2])
+    shipshape_Y   = np.array([-lpp / 2, lpp / 4, lpp / 2, lpp / 4, -lpp / 2, -lpp / 2])
+    shipshape_X   = np.array([-breadth / 2, -breadth / 2, 0, breadth / 2, breadth / 2, -breadth / 2])
+    pole_r        = np.empty(6)
+    pole_theta    = np.empty(6)
+    pole_r[:]     = (shipshape_X[:] ** 2 + shipshape_Y[:] ** 2) ** (1 / 2)
+    pole_theta[:] = np.arctan2(shipshape_Y[:],shipshape_X[:])
+
+    x_fix = np.cos(pole_theta[:] + psi) * pole_r[:] + x
+    y_fix = np.sin(pole_theta[:] + psi) * pole_r[:] + y
+    
+    return x_fix, y_fix
